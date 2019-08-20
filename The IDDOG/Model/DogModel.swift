e12b4013d0856975f7a9c8e482d4898a9faad1c7
Matheus Holanda <matheus.albuquerque@agilecontent.com>
2019-08-20 16:57:10 -0300

    Navigation Bar and models added.

---
+++ The IDDOG/Model/DogModel.swift
@@ -0,0 +1,17 @@
+//
+//  DogModel.swift
+//  The IDDOG
+//
+//  Created by Matheus Holanda Pinheiro de Albuquerque on 20/08/19.
+//  Copyright © 2019 Matheus Holanda. All rights reserved.
+//
+
+import Foundation
+
+open class DogModel {
+    
+    public var category: String?
+    public var imagesList: [String]?
+    
+    public init(){}
+}
